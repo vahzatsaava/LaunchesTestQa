2ab750c15afd620b2513c1d103e9263f39450cbd
VazhaTsaava <vazhatsaava@gmail.com>
2026-01-10 13:35:03 +0400

    refactor

--- src/main/java/framework/core/driver/DriverManager.java
+++ src/main/java/framework/core/driver/DriverManager.java
@@ -3,17 +3,27 @@ package framework.core.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
-    private DriverManager() {
-    }
+    private DriverManager() {}
 
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static void init() {
         WebDriverManager.chromedriver().setup();
-        driver.set(new ChromeDriver());
-        driver.get().manage().window().maximize();
+
+        ChromeOptions options = new ChromeOptions();
+
+        // 👉 GitHub Actions / CI
+        if (isCI()) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        driver.set(new ChromeDriver(options));
     }
 
     public static WebDriver getDriver() {
@@ -21,7 +31,13 @@ public class DriverManager {
     }
 
     public static void quit() {
-        driver.get().quit();
-        driver.remove();
+        if (driver.get() != null) {
+            driver.get().quit();
+            driver.remove();
+        }
+    }
+
+    private static boolean isCI() {
+        return "true".equalsIgnoreCase(System.getenv("CI"));
     }
 }
